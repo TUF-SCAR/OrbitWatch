d2c1bf46ab0037956401c1f25fe0faf521c2d110
TUF_SCAR <tufscar69@gmail.com>
2026-07-28 23:26:32 +0530

    adding frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <header>
+      <h1>OrbitWatch</h1> <br />
+      <p>Live Satellite Tracking Platform</p>
+    </header>
+  );
+}
+
+export default App;

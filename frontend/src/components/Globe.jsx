a07daaae92317c6e61a3ed5f2ab4664de2198c63
TUF_SCAR <tufscar69@gmail.com>
2026-08-04 22:55:18 +0530

    feat: continuously refresh satellite trajectory

--- frontend/src/components/Globe.jsx
+++ frontend/src/components/Globe.jsx
@@ -6,24 +6,12 @@ import {
   Color,
   SampledPositionProperty,
   JulianDate,
+  TimeInterval,
+  Iso8601,
 } from "cesium";
 import "./Globe.css";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 const token = import.meta.env.VITE_CESIUM_ION_TOKEN;
-
-async function loadItem() {
-  const response = await fetch(
-    "http://127.0.0.1:8000/api/satellites/25544/position",
-  );
-
-  if (!response.ok) {
-    throw new Error(`Request failed: ${response.status}`);
-  }
-
-  const item = await response.json();
-
-  return item;
-}
 
 async function loadTrajectoryItem() {
   const response = await fetch(
@@ -46,11 +34,10 @@ function Globe() {
     Ion.defaultAccessToken = token;
     const globeViewer = new Viewer(areaRef.current);
     let satelliteMarker = null;
+    const satellitePositions = new SampledPositionProperty();
 
     async function updateSatelliteTrajectory() {
       const satelliteTrajectory = await loadTrajectoryItem();
-
-      const satellitePositions = new SampledPositionProperty();
 
       for (const position of satelliteTrajectory.positions) {
         const timestamp = JulianDate.fromIso8601(position.timestamp);
@@ -69,75 +56,45 @@ function Globe() {
         satellitePositions.addSample(timestamp, satellitePosition);
       }
 
-      const satelliteMarker2 = globeViewer.entities.add({
-        position: satellitePositions,
-        point: {
-          pixelSize: 14,
-          color: Color.GRAY,
-        },
-        label: {
-          text: `${satelliteTrajectory.name} - (${satelliteTrajectory.norad_id})`,
-        },
+      const positionsCutoff = JulianDate.addSeconds(
+        globeViewer.clock.currentTime,
+        -30,
+        new JulianDate(),
+      );
+
+      const oldPositions = new TimeInterval({
+        start: Iso8601.MINIMUM_VALUE,
+        stop: positionsCutoff,
       });
-      globeViewer.clock.currentTime = JulianDate.fromIso8601(
-        satelliteTrajectory.positions[0].timestamp,
-      );
-      globeViewer.clock.shouldAnimate = true;
-      globeViewer.zoomTo(satelliteMarker2);
 
-      console.log(satellitePositions);
-    }
-
-    async function getSatellitePosition() {
-      const satellite = await loadItem();
-
-      if (!satellite) {
-        console.warn("No satellite data recieved");
-        return;
-      }
-
-      const { latitude, longitude, altitude_km, name, norad_id } = satellite;
-      const height = altitude_km * 1000;
-
-      const satellitePosition = Cartesian3.fromDegrees(
-        longitude,
-        latitude,
-        height,
-      );
-
-      return {
-        satellitePosition,
-        name,
-        norad_id,
-      };
-    }
-
-    async function updateSatellitePosition() {
-      const { satellitePosition, name, norad_id } =
-        await getSatellitePosition();
+      satellitePositions.removeSamples(oldPositions);
 
       if (!satelliteMarker) {
         satelliteMarker = globeViewer.entities.add({
-          position: satellitePosition,
+          position: satellitePositions,
           point: {
             pixelSize: 14,
-            color: Color.YELLOW,
+            color: Color.GRAY,
           },
           label: {
-            text: `${name} - (${norad_id})`,
+            text: `${satelliteTrajectory.name} - (${satelliteTrajectory.norad_id})`,
           },
         });
+        globeViewer.clock.currentTime = JulianDate.fromIso8601(
+          satelliteTrajectory.positions[0].timestamp,
+        );
+        globeViewer.clock.shouldAnimate = true;
         globeViewer.zoomTo(satelliteMarker);
-      } else {
-        satelliteMarker.position.setValue(satellitePosition);
       }
     }
     updateSatelliteTrajectory();
-    //updateSatellitePosition();
-    //const updateSatelliteInterval = setInterval(updateSatellitePosition, 5000);
+    const satelliteTrajectoryInterval = setInterval(
+      updateSatelliteTrajectory,
+      60000,
+    );
 
     return () => {
-      //clearInterval(updateSatelliteInterval);
+      clearInterval(satelliteTrajectoryInterval);
       globeViewer.destroy();
     };
   }, []);
